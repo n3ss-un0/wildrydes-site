3eb2cc5d6cdefaa17bb323b61de50cd7a4ee2993
n3ss-un0 <103532303+n3ss-un0@users.noreply.github.com>
2025-02-05 10:44:06 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-west-2_6hWAOGZIP', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'vovqqt6p74rgjdpm8c4prs6u9', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
